3076b4b3fc340d994978a450193630c002d1307f
Mohamed Gomaa <m.gomaa.abd.mm@gmail.com>
2024-05-23 18:54:00 +0300

    Defined Jewellery Item Model

--- rubies_jewels/store/models.py
+++ rubies_jewels/store/models.py
@@ -5,3 +5,13 @@ class Email(models.Model):
 
     def __str__(self):
         return self.email
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
+    image_url = models.URLField(null=False, blank=False)
+    stock = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
